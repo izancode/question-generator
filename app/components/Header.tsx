84a93da9217e80d7b9fad5a756e6d5696459bce7
izancode <sf48116@gmail.com>
2025-10-11 15:23:45 +0530

    11 oct afternoon

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export  const Header = () => {
   return (
-  <header>
+  <header className=''>
     one1
   </header>
   )
--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-//    images: {
-//     domains: ["images.unsplash.com", "example.com"],
-//     formats: ["image/avif", "image/webp"],
-//   },
-//   env: {
-//     API_URL: process.env.API_URL,
-//   },
-
 };
 
 export default nextConfig;
