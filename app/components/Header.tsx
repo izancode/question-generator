14a056b6b1160453171b27e53d67b4b68b25132d
izancode <sf48116@gmail.com>
2025-10-12 21:34:06 +0530

    12 oct night

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Image from 'next/image'
 
 export  const Header = () => {
   return (
-  <header className=''>
-    one1
+    <div>
+  <header className='border-4 border-indigo-500 bg-white fixed left-1/2 -translate-x-1/2 top-0 h-20 w-full max-w-[1100px]'>
+  <Image src="/logo.svg" alt="Logo" width={150} height={50} className='h-12 w-auto mt-4 ml-4'/>
   </header>
+
+  <p className='h-[1000px]'></p>
+  </div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import { Header } from "./components/Header";
+
 export default function Home() {
   return (
 
 <div>
          <Header />
+        
+         
 </div>
   );
 }
